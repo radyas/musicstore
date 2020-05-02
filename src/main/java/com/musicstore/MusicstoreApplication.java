b2abff3b3deb9490aa87f053142d6585f6f4c1ef
radyas <yasirusellahewage@gmail.com>
2020-05-02 14:29:26 +0530

    Initial Commit

---
+++ src/main/java/com/musicstore/MusicstoreApplication.java
@@ -0,0 +1,13 @@
+package com.musicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicstoreApplication.class, args);
+    }
+
+}
